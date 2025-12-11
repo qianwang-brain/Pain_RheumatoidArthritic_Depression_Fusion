197b3ea7ffa53bfae8c71b0dd71a8dbe24d28de4
Mia <73322269+qianwang-brain@users.noreply.github.com>
2025-12-11 10:09:30 +0800

    5_MR_plot_forest.R

--- Codes/5_MR_plot_forest.R
+++ Codes/5_MR_plot_forest.R
@@ -14,11 +14,9 @@ library(data.table)
 library(forestplot)
 library(dplyr)
 
-# 读取数据（示例路径需替换为实际路径）
 data <- fread("E:/Project_R/R_code/UKB_project_RA/RA_MR_analysis/results_protein2/pval_matched_rows_Final.csv", 
               header = TRUE)
 
-# 数据格式化
 combined_data <- data %>%
   select(exposure,outcome, or, or_lci95, or_uci95, pval) %>%
   mutate(
@@ -26,7 +24,6 @@ combined_data <- data %>%
     p_value = formatC(pval, format = "e", digits = 2)
   )
 
-# 添加表头（网页1方法）
 header <- tibble(
   exposure = "exposure",
   outcome="outcome",
@@ -36,17 +33,16 @@ header <- tibble(
 combined_data <- bind_rows(header, combined_data)
 
 cairo_pdf("E:/Project_R/R_code/UKB_project_RA/RA_MR_analysis/forestplot_ALL.pdf", 
-          width = 8,       # 宽度12英寸（A4纸宽度）
-          height = 11,       # 高度8英寸
-          family = "Arial") # 设置英文字体
+          width = 8,     
+          height = 11,      
+          family = "Arial")
 
-# 绘制森林图（网页2的绘图参数优化）
 forestplot(
   labeltext = as.matrix(combined_data[, c("exposure","outcome","OR (95% CI)", "p_value")]),
   mean = c(NA, combined_data$or[-1]),
   lower = c(NA, combined_data$or_lci95[-1]),
   upper = c(NA, combined_data$or_uci95[-1]),
-  graph.pos = 4,  # 调整图形位置到第4列[6](@ref)
+  graph.pos = 4, 
   boxsize = 0.35,
   zero = 1,
   xlog = FALSE,
@@ -64,6 +60,7 @@ forestplot(
 )
 
 dev.off() 
+
 
 
 

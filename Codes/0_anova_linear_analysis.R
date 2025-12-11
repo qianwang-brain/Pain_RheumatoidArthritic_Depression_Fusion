93fb11384c2f41d84cc3658f8f35514b2cde78e2
Mia <73322269+qianwang-brain@users.noreply.github.com>
2025-12-11 10:05:26 +0800

    0_anova_linear_analysis.R

--- Codes/0_anova_linear_analysis.R
+++ Codes/0_anova_linear_analysis.R
@@ -14,7 +14,7 @@
 #   – Performs a one-way ANOVA to evaluate overall group differences.
 #   – Applies polynomial contrasts (linear, quadratic, cubic) to quantify
 #        systematic trends across disease burden levels.
-#   – Extracts linear / quadratic / cubic trend statistics from the model
+#   – Extracts linear/quadratic/cubic trend statistics from the model
 #        using summary.lm for clearer interpretation of gradient effects.
 #
 # Reference:
@@ -25,27 +25,24 @@
 library(readr)
 library(dplyr)
 
-## 1) 读入数据 -------------------------------------------------------------
 df <- read_csv("C:/Program Files/MATLAB/R2020b/bin/ukb_RA/pain_guided_blood/3_MCCAR2_results_2/results_plot/Linear_gradient/Blood1_ttest_all.csv")
 
 df$group <- factor(
   trimws(df$group),
-  levels = c("healthy","Depression","RA","Comorbid"),
+  levels = c("healthy", "Depression", "RA" ,"Comorbid"),
   ordered = TRUE
 )
 
-## 2) 单因素 ANOVA（总体差异）---------------------------------------------
 anova_res <- aov(a1_h ~ group, data = df)
 summary(anova_res)
 
 
-## 3) 多项式趋势分析（polynomial contrasts）-------------------------------
-##    把 group 设为正交多项式对比：L(线性)、Q(二次)、C(三次) 等
+## 3) polynomial contrasts-------------------------------
 contrasts(df$group) <- contr.poly(nlevels(df$group))
 anova_trend <- aov(a1_h ~ group, data = df)
 
-## (1) 整体 ANOVA
+## (1) ANOVA
 summary(anova_trend)
-
-## (2) 提取线性 / 二次 / 三次项的 t, p（更清楚地写在线性趋势里）
+## (2)
 summary.lm(anova_trend)
+
